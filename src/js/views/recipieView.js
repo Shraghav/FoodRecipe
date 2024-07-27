cf020e1bd37b7b1f0813a0137b5d960e88eeee0e
Shraghav <harishragavan04@gmail.com>
2024-07-27 11:57:52 +0530

    fixed error in controller

--- src/js/views/recipieView.js
+++ src/js/views/recipieView.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import View from './View';
+import { numberToFraction } from '../helpers';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = "Nothing is found. Try again";
@@ -88,7 +89,7 @@ class RecipeView extends View {
                     <svg class="recipe__icon">
                     <use href="${icons}#icon-check"></use>
                     </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ""}</div>
+                <div class="recipe__quantity">${ing.quantity ? numberToFraction(ing.quantity).toString() : '' }</div>
                 <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
